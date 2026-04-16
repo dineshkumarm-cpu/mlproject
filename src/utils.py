edee8050ddd5699aa1ef2fddba05351ebaf387ca
dinesh <dineshkumarmurugan2005@gmail.com>
2026-04-16 23:34:35 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,10 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -19,3 +23,32 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+def evaluate_models(X_train, y_train, X_test, y_test, models, param=None):
+    try:
+        report = {}
+        param = param or {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param.get(model_name, {})
+
+            if para:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
